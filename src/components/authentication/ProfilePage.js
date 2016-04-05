431c87b484dbbe5bdd7cc958865f9db063a88df3
Alex Brylov <oobrylov@ucalgary.ca>
2016-04-04 22:55:02 -0600

    made profile

--- src/components/authentication/ProfilePage.js
+++ src/components/authentication/ProfilePage.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
 import { UserProfileForm } from 'react-stormpath';
+import {Tabs, Tab, Thumbnail, Button} from 'react-bootstrap';
 
-export default class ProfilePage extends React.Component {  
+export default class ProfilePage extends React.Component {
   render() {
     return (
       <DocumentTitle title={`My Profile`}>
@@ -15,7 +16,87 @@ export default class ProfilePage extends React.Component {
           </div>
           <div className="row">
             <div className="col-xs-12">
-              <UserProfileForm />
+
+            <h4>Name: User </h4>
+            <h4>Email: email </h4>
+            <h4 bsStyle="bottom-padding:40px;">License: ### </h4>
+
+            <div>
+          <Tabs defaultActiveKey={1}>
+              <Tab eventKey={1} title="Inbox">
+
+              <Thumbnail>
+                <h3>Dude I want your car!</h3>
+                <p>I want to rent it bad for Nelson!</p>
+                <p>
+                  <Button bsStyle="primary">Reply</Button>&nbsp;
+                  <Button bsStyle="default">Delete</Button>
+                </p>
+              </Thumbnail>
+
+              <Thumbnail>
+                <h3>Dude I want your car!</h3>
+                <p>I want to rent it bad for Nelson!</p>
+                <p>
+                  <Button bsStyle="primary">Reply</Button>&nbsp;
+                  <Button bsStyle="default">Delete</Button>
+                </p>
+              </Thumbnail>
+
+              <Thumbnail>
+                <h3>Dude I want your car!</h3>
+                <p>I want to rent it bad for Nelson!</p>
+                <p>
+                  <Button bsStyle="primary">Reply</Button>&nbsp;
+                  <Button bsStyle="default">Delete</Button>
+                </p>
+              </Thumbnail>
+
+              </Tab>
+
+              <Tab eventKey={2} title="Reviews">
+
+            <Thumbnail>
+              <h3>Thumbnail label</h3>
+              <p>Amazing</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+
+            <Thumbnail>
+              <h3>Thumbnail label</h3>
+              <p>Amazing</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+
+            <Thumbnail>
+              <h3>Thumbnail label</h3>
+              <p>Amazing</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+
+            <Thumbnail>
+              <h3>Thumbnail label</h3>
+              <p>Amazing</p>
+              <p>
+                <Button bsStyle="primary">Button</Button>&nbsp;
+                <Button bsStyle="default">Button</Button>
+              </p>
+            </Thumbnail>
+              </Tab>
+
+              <Tab eventKey={3} title="Update Profile"><UserProfileForm /></Tab>
+            </Tabs>
+            </div>
+
             </div>
           </div>
         </div>
