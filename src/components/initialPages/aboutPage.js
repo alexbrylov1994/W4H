ae1de6aa00f834c586587d7a37a136ae9dbe5afa
Alex Brylov <oobrylov@ucalgary.ca>
2016-04-12 08:47:02 -0600

    working on about page

--- src/components/initialPages/aboutPage.js
+++ src/components/initialPages/aboutPage.js
@@ -8,27 +8,40 @@ import { LoginLink } from 'react-stormpath';
 export default class aboutPage extends React.Component {
 
   render() {
+
+    var picStyle =
+    {
+      width: '900px',
+      height: '500px',
+      align: 'center',
+      margin: '0 auto'
+    };
+
     return (
       <div className="container">
         <h3 className="text-center">Welcome W4Hire users!</h3>
         <hr />
+
+        <div className="container-fluid">
         <Carousel>
           <CarouselItem>
-            <img width={400} height={500} alt="400x500" align="center" src="http://www.extremetech.com/wp-content/uploads/2015/04/inter-IP.jpg"/>
+
+            <img className="img-responsive" style={picStyle} alt="car" src="http://www.extremetech.com/wp-content/uploads/2015/04/inter-IP.jpg"/>
             <div className="carousel-caption">
               <h3>Rent Car</h3>
               <p>The ultimate place for renting cars and have a comfortable, affordable ride</p>
             </div>
+
           </CarouselItem>
           <CarouselItem>
-            <img width={400} height={500} alt="400x500"  align="center" src="http://www.extremetech.com/wp-content/uploads/2015/04/inter-IP.jpg"/>
+            <img className="img-responsive" style={picStyle} alt="money" src="http://salesman.red/wp-content/uploads/2016/01/money-background-imagesearn-money-online-7a4zz8nx.jpg"/>
             <div className="carousel-caption">
               <h3>Make Money</h3>
               <p>This application allows you to make money with a minimum ammount of effort</p>
             </div>
           </CarouselItem>
           <CarouselItem>
-            <img width={400} height={500} alt="400x500" align="middle"  src="./money.png"/>
+            <img className="img-responsive" style={picStyle} alt="safety" src="http://www.adamsindustriesinc.com/wp-content/uploads/2016/02/safety-first2.jpg"/>
             <div className="carousel-caption">
               <h3>Safety</h3>
               <p>We got you covered. WE know, accidents happen, and we are there to make
@@ -36,6 +49,7 @@ export default class aboutPage extends React.Component {
             </div>
           </CarouselItem>
         </Carousel>
+        </div>
       </div>
     );
   }
