5fd875c08f746c2f639b16941d3b3609780b78b7
namuthan <nabilbbb33@gmail.com>
2016-04-12 12:28:57 -0600

    added register label

--- src/components/authentication/LoginPage.js
+++ src/components/authentication/LoginPage.js
@@ -4,7 +4,25 @@ import { LoginForm, RegistrationForm, LoginLink } from 'react-stormpath';
 import {Tabs, Tab} from 'react-bootstrap';
 
 export default class LoginPage extends React.Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state =  {
+      inLogin : "Login"
+    };
+  }
+
+  changeText(e){
+      if(e == 1){
+        this.setState({inLogin: "Login"});
+      } else{
+        this.setState({inLogin: "Register"});
+      }
+  }
+
   render() {
+
     var loginFormStyle = {
       background:'rgba(0, 0, 0, 0.17)',
       padding:'20px',
@@ -16,11 +34,11 @@ export default class LoginPage extends React.Component {
         <div className="container" style={loginFormStyle}>
           <div className="row">
             <div className="col-xs-12">
-              <h3>Login</h3>
+              <h3>{this.state.inLogin}</h3>
             </div>
           </div>
 
-          <Tabs justified defaultActiveKey={1}>
+          <Tabs justified defaultActiveKey={1} onSelect={this.changeText.bind(this)}>
           <Tab eventKey={1} title="Login"><p bsStyle="padding:40px;"></p><br/><LoginForm/></Tab>
           <Tab eventKey={2} title="Register"><p bsStyle="padding:40px;"></p><br/><RegistrationForm/></Tab>
           </Tabs>
