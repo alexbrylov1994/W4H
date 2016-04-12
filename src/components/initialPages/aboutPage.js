708488e086f9539d80156bde7c004b58fcaf1433
Alex Brylov <oobrylov@ucalgary.ca>
2016-04-12 09:19:30 -0600

    did more stuff for about page

--- src/components/initialPages/aboutPage.js
+++ src/components/initialPages/aboutPage.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router';
 import React, { PropTypes } from 'react';
-import {Carousel, CarouselItem} from 'react-bootstrap';
+import {Carousel, CarouselItem, Jumbotron, Col} from 'react-bootstrap';
 import DocumentTitle from 'react-document-title';
 
 import { LoginLink } from 'react-stormpath';
@@ -17,10 +17,18 @@ export default class aboutPage extends React.Component {
       margin: '0 auto'
     };
 
+    var padStyle=
+    {
+      paddingTop:'55px'
+    };
+
     return (
       <div className="container">
+
+      <Jumbotron>
         <h3 className="text-center">Welcome W4Hire users!</h3>
-        <hr />
+        <p className="text-center"> Prepare to get amazed at how easy everything is!</p>
+      </Jumbotron>
 
         <div className="container-fluid">
         <Carousel>
@@ -49,6 +57,57 @@ export default class aboutPage extends React.Component {
             </div>
           </CarouselItem>
         </Carousel>
+
+        <div style={padStyle}> </div>
+
+        <Col xs={12}>
+        <div>
+
+          <h3 className="text-center"> History </h3>
+          <p className="text-center"> W4Hire was created by 6 ambitious entrepreneurs who wanted to introduce the world cheap, easy transportation and a way to make money.
+          We live in a time or recession and  some people are in a deed of money and out platform can help those people to have an extra source of income.
+          We want to make this world a better place and make it more affordable for everyone.
+          </p>
+
+        </div>
+        </Col>
+
+        <div style={padStyle}> </div>
+
+        <Col xs={12} sm={12} md={4} lg={4}>
+        <div>
+          <h3 className="text-center"> Rent Cars </h3>
+          <p> Want to have an affordable transportation?
+              We got your back! We are providing most affordable prices
+              on the market. Who needs taxi drivers or buying a car when you can
+              rent a car for a small fee and get to any location you want?
+          </p>
+        </div>
+        </Col>
+
+        <Col xs={12} sm={12} md={4} lg={4}>
+        <div>
+          <h3 className="text-center"> Make Money </h3>
+          <p> We are living in a time of recession and everyone need some extra money.
+              You are a college student? Don’t have a steady job?
+              Unemployed? Want to have enough money for a vacation?
+              Want to start your own startup? Then we got your back, with
+              our platform you can make lots of money with a minimal amount of effort!
+          </p>
+        </div>
+        </Col>
+
+        <Col xs={12} sm={12} md={4} lg={4}>
+        <div>
+          <h3 className="text-center"> Stay safe </h3>
+          <p> No one is safe those days, especially when you have deal with other clients.
+              We aware that your car can be stolen, damaged, etc. And guess what?
+              We got you covered! We take security precautions to ensure your safety
+              and if something happened to your car, your problems will be fixed in no time!
+          </p>
+        </div>
+        </Col>
+
         </div>
       </div>
     );
