3688e51877cebe292e3b0d3bc36fe2a4baf37dfd
Alex Brylov <oobrylov@ucalgary.ca>
2016-04-11 22:23:20 -0600

    saving progress

--- src/components/initialPages/carList.js
+++ src/components/initialPages/carList.js
@@ -18,14 +18,20 @@ export default class carList extends React.Component {
 
   var picStyle =
   {
-    width: '350px',
-    height: '350px',
-    align: 'right'
+    width: '300px',
+    height: '300px',
+    align: 'left'
   };
 
   var textStyle =
   {
     align:'right'
+  };
+
+  var tnailStyle=
+  {
+    width: '90%',
+    align:'center'
   };
 
 
@@ -41,19 +47,77 @@ export default class carList extends React.Component {
           </div>
 
         <div>
-        <Button bsSize="large"><Glyphicon glyph="minus"/>Add</Button>
+        <Button bsSize="large"><Glyphicon glyph="plus"/>Add</Button>
         </div>
 
-        <div>
+        <div className="container-fluid">
+        <Thumbnail style={tnailStyle}>
 
-        <Thumbnail>
-
+        <div className="container-fluid">
         <Col xs={12} sm={12} md={6} lg={6}>
-        <Image src="http://blog.caranddriver.com/wp-content/uploads/2015/03/2017-Ford-GT-301-876x535.jpg" style={picStyle}/>
+        <Image className="img-responsive" src="http://blog.caranddriver.com/wp-content/uploads/2015/03/2017-Ford-GT-301-876x535.jpg" style={picStyle}/>
         </Col>
 
         <Col xs={12} sm={12} md={6} lg={6}>
           <h3>Ferrari 458 Italia (2009)</h3>
+          <p style={textStyle} >Manual</p>
+          <p>Price: 60$/hr</p>
+        </Col>
+
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <p>
+            <Button bsStyle="primary"><Glyphicon glyph="eye-open"/> View</Button>
+            <Button bsStyle="warning"><Glyphicon glyph="edit"/> Edit</Button>
+            <Button bsStyle="danger"><Glyphicon glyph="remove"/> Remove</Button>
+
+          </p>
+        </Col>
+        </div>
+
+        </Thumbnail>
+        </div>
+
+
+
+        <div className="container-fluid">
+        <Thumbnail style={tnailStyle}>
+
+        <div className="container-fluid">
+        <Col xs={12} sm={12} md={6} lg={6}>
+        <Image className="img-responsive" src="http://blog.caranddriver.com/wp-content/uploads/2015/03/2017-Ford-GT-301-876x535.jpg" style={picStyle}/>
+        </Col>
+
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <h3>Ferrari 458 Italia (2009)</h3>
+          <p style={textStyle}>Manual</p>
+          <p>Price: 60$/hr</p>
+        </Col>
+
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <p>
+            <Button bsStyle="primary"><Glyphicon glyph="eye-open"/> View</Button>
+            <Button bsStyle="warning"><Glyphicon glyph="edit"/> Edit</Button>
+            <Button bsStyle="danger"><Glyphicon glyph="remove"/> Remove</Button>
+
+          </p>
+        </Col>
+        </div>
+
+        </Thumbnail>
+        </div>
+
+
+
+        <div className="container-fluid">
+        <Thumbnail style={tnailStyle}>
+
+        <div className="container-fluid">
+        <Col xs={12} sm={12} md={6} lg={6}>
+        <Image className="img-responsive" src="http://blog.caranddriver.com/wp-content/uploads/2015/03/2017-Ford-GT-301-876x535.jpg" style={picStyle}/>
+        </Col>
+
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <h3>Manual</h3>
           <p style={textStyle} >Ferrari 458 Italia (2009)</p>
           <p>Price: 60$/hr</p>
         </Col>
@@ -66,10 +130,11 @@ export default class carList extends React.Component {
 
           </p>
         </Col>
+        </div>
 
         </Thumbnail>
-
         </div>
+
 
 
         </div>
