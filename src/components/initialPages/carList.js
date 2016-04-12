d14168e09d7f7c38590a60451eb97fe95434bb58
Alex Brylov <oobrylov@ucalgary.ca>
2016-04-11 20:59:00 -0600

    progress save

--- src/components/initialPages/carList.js
+++ src/components/initialPages/carList.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
 import { UserProfileForm } from 'react-stormpath';
-import {Tabs, Tab, Thumbnail, Button, Glyphicon} from 'react-bootstrap';
+import {Tabs, Tab, Thumbnail, Button, Glyphicon, Image, Col} from 'react-bootstrap';
 
 export default class carList extends React.Component {
 
+
   render() {
+
+  var buttonStyle =
+  {
+    width: '100px',
+    height: '40px',
+    background:'#bbb',
+    color: '#999'
+  };
+
+  var picStyle =
+  {
+    width: '350px',
+    height: '350px',
+    align: 'right'
+  };
+
+  var textStyle =
+  {
+    align:'right',
+    paddingLeft: '90%'
+  };
+
 
     return (
       <DocumentTitle title={`Garage`}>
@@ -19,27 +42,36 @@ export default class carList extends React.Component {
           </div>
 
         <div>
-        <Button bsSize="large"><Glyphicon glyph="plus" />Add</Button>
+        <Button bsSize="large"><Glyphicon glyph="plus"/>Add</Button>
         </div>
 
         <div>
 
-        <Thumbnail src="http://blog.caranddriver.com/wp-content/uploads/2015/03/2017-Ford-GT-301-876x535.jpg" bsSize="small">
+        <Thumbnail>
 
-          <h3>Thumbnail label</h3>
-          <p>Ferrari 458 Italia (2009)</p>
+        <Col xs={12} sm={12} md={6} lg={6}>
+        <Image src="http://blog.caranddriver.com/wp-content/uploads/2015/03/2017-Ford-GT-301-876x535.jpg" style={picStyle}/>
+        </Col>
+
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <h3>Ferrari 458 Italia (2009)</h3>
+          <p style={textStyle} >Ferrari 458 Italia (2009)</p>
           <p>Price: 60$/hr</p>
+        </Col>
 
+        <Col xs={12} sm={12} md={6} lg={6}>
           <p>
-            <Button bsStyle="primary">Button</Button>
-            <Button bsStyle="warning">Button</Button>
-            <Button bsStyle="danger">Button</Button>
+            <Button bsStyle="primary"><Glyphicon glyph="eye-open"/> View</Button>
+            <Button bsStyle="warning"><Glyphicon glyph="edit"/> Edit</Button>
+            <Button bsStyle="danger"><Glyphicon glyph="remove"/> Remove</Button>
 
           </p>
+        </Col>
+
         </Thumbnail>
 
         </div>
-
+      <p>  </p>
 
         </div>
       </DocumentTitle>
